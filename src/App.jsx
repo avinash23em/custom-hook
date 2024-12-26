f4b2b06b5716207d0966e71d5d50398150a46c73
avinash23em <ap23vi@gmail.com>
2024-12-26 18:59:44 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { act, useState } from 'react'
 
 import './App.css'
 import useFetch from './Hooks/Usefetch'
+import { Useprev } from './Hooks/Useprev';
 // function useCounter(){
 //   const[count,setcount]=useState(0);
 //   function increase(){
@@ -33,23 +34,44 @@ import useFetch from './Hooks/Usefetch'
 //     </>
 //   )
 // }
+
+// usefetch implementation 
+
+
+// function App(){
+//    const[page,setpage]=useState(1);
+//   const{post,load}=useFetch("https://jsonplaceholder.typicode.com/posts/"+page);
+//   if(load){
+//     return (
+//       <div>
+//       loading......
+//       </div>
+//     )
+//   }
+//   return (
+//   <div>
+//   <button onClick={()=> setpage(1)}>page 1</button>
+//   <button onClick={()=> setpage(2)}>page 2</button>
+//   <button onClick={()=> setpage(3)}>page 3</button>
+
+//     {JSON.stringify(post)}
+//   </div>
+// )
+// }
+
+
+//use prev implementation 
+
 function App(){
-   const[page,setpage]=useState(1);
-  const{post,load}=useFetch("https://jsonplaceholder.typicode.com/posts/"+page);
-  if(load){
-    return (
-      <div>
-      loading......
-      </div>
-    )
-  }
-  return (
-  <div>
-  <button onClick={()=> setpage(1)}>page 1</button>
-  <button onClick={()=> setpage(2)}>page 2</button>
-  <button onClick={()=> setpage(3)}>page 3</button>
+  const[curr,setcurr]=useState(0);
+  const prev=Useprev(curr);
   
-    {JSON.stringify(post)}
+return(
+  <div>
+    <button onClick={()=>{
+      setcurr((curr)=>curr+1);
+    }}>click me {curr}</button>
+    <p>the previous value was {prev}</p>
   </div>
 )
 }
